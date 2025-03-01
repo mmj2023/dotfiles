eff32905c6b483e98e7c1e0c30a7c85ee50f1338
mmj <121494430+mmj2023@users.noreply.github.com>
2025-03-01 07:09:46 -0500

    changed options for linting

--- freshnvim/.config/freshnvim/lua/plugins/linting.lua
+++ freshnvim/.config/freshnvim/lua/plugins/linting.lua
@@ -27,24 +27,25 @@ return {
         python = { "flake8" },
         javascript = { "eslint" },
         lua = { "luacheck" },
+        markdown = { "typos" }, -- Enable `typos` for Markdown files
+        text = { "typos" }, -- Enable `typos` for text files
+        txt = { "typos" }, -- Enable `typos` for `.txt` files
         -- fish = { "fish" },
         -- Use the "*" filetype to run linters on all filetypes.
         -- ['*'] = { 'global linter' },
         -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
         -- ['_'] = { 'fallback linter' },
-        ["*"] = { "typos" },
       },
-      -- LazyVim extension to easily override linter options
-      -- or add custom linters.
       ---@type table<string,table>
       linters = {
         -- -- Example of using selene only when a selene.toml file is present
         selene = {
-          --   -- `condition` is another LazyVim extension that allows you to
-          --   -- dynamically enable/disable linters based on the context.
           condition = function(ctx)
             return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
           end,
+        },
+        typos = {
+          args = { "--lang", "en" }, -- Only for English-based files
         },
       },
     },
