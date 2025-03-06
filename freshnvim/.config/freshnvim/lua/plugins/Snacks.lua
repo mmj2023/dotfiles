73c9eca5455c940ca82d156fea542b87cc0def16
mmj <121494430+mmj2023@users.noreply.github.com>
2025-03-06 01:32:51 -0500

    bug fixes

--- freshnvim/.config/freshnvim/lua/plugins/Snacks.lua
+++ freshnvim/.config/freshnvim/lua/plugins/Snacks.lua
@@ -1,4 +1,12 @@
-
+-- Terminal Mappings
+local function term_nav(dir)
+  ---@param self snacks.terminal
+  return function(self)
+    return self:is_floating() and "<c-" .. dir .. ">" or vim.schedule(function()
+      vim.cmd.wincmd(dir)
+    end)
+  end
+end
 return {
     "folke/snacks.nvim",
     opts = {
