73fba90c79d91b98f8dc4caf82b7d01aee4eeb40
mmj <121494430+mmj2023@users.noreply.github.com>
2025-03-11 04:43:42 -0400

    git integration with oil.nvim

--- freshnvim/.config/freshnvim/lua/plugins/oil.lua
+++ freshnvim/.config/freshnvim/lua/plugins/oil.lua
@@ -1,6 +1,15 @@
 return {
-    'stevearc/oil.nvim',
-    event = 'VeryLazy',
+  {
+    "FerretDetective/oil-git-signs.nvim",
+    ft = "oil",
+    ---@module "oil_git_signs"
+    ---@type oil_git_signs.Config
+    opts = {},
+  },
+
+  {
+    "stevearc/oil.nvim",
+    event = "VeryLazy",
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {
@@ -9,10 +18,20 @@ return {
       watch_for_changes = true,
       view_options = {
         -- Show files and directories that start with "."
-            show_hidden = true,
-        }
+        show_hidden = true,
+      },
+      win_options = {
+        signcolumn = "yes:2",
+        statuscolumn = "",
+      },
     },
     -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", --[[ opts = {} ]] } },
+    dependencies = {
+      {
+        "echasnovski/mini.icons", --[[ opts = {} ]]
+        "FerretDetective/oil-git-signs.nvim",
+      },
+    },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  },
 }
